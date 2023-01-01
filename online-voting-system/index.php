7920fe16d3855b3b72ca47121c6bd0afa6d5ec17
nil000y <119658946+nil000y@users.noreply.github.com>
2023-01-01 21:54:20 +0530

    this is my online voting website

---
+++ online-voting-system/index.php
@@ -0,0 +1,38 @@
+</html>
+    <head>
+        <title>Online voting system - Home</title>
+        <link rel="stylesheet" href="css/stylesheet.css">
+
+        <center>
+
+
+               
+            <div id="headerSection">
+                <h1>ONLINE VOTING SYSTEM<h1> 
+            </div>
+            
+          
+</html>
+<div class="box">
+    <marquee behavior="scroll" direction="left">The Voting polls are open now!!VOTE EARLY!</marquee>
+
+            <div id="loginSection">
+                <h2>Login</h2>
+                <form action="api/login.php" method="POST">
+                    <input type="number" name="mob" placeholder="Enter mobile" pattern="[1-10]*" required><br><br>
+                    <input type="password" name="pass" placeholder="Enter password" required><br><br>
+                    <select name="role" style="width: 10%; height: 1cm; border: 2px solid black">
+                        <option value="1">Voter</option>
+                        <option value="2">Candidate</option>
+                    </select><br><br>                  
+                    <button id="loginbtn" type="submit" name="loginbtn">Login</button><br><br>
+                    New user? <a href="routes/register.php">Register here</a>
+                   
+                    <div class="menu-section">
+                    <head>
+
+</style>
+ 
+</body>
+</html>
+
